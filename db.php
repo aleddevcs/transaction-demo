aa77705268c814dbf65824889954870239a7ab0f
aleddevcs <aevans1@cavc.ac.uk>
2026-06-03 14:32:58 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,13 @@
 
+<?php
+$host = "db";
+$user = "root";
+$password = "root";
+$database = "transaction_demo";
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
