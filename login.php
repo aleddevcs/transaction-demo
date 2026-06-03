85573d0deadd080107b5d06fbe24dd8583453608
aleddevcs <aevans1@cavc.ac.uk>
2026-06-03 14:33:40 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,48 @@
+<?php
+session_start();
+include "db.php";
 
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        $_SESSION["username"] = $username;
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        $error = "Invalid username or password.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+
+    <form method="POST">
+        <label>Username:</label>
+        <input type="text" name="username">
+        <br><br>
+
+        <label>Password:</label>
+        <input type="password" name="password">
+        <br><br>
+
+        <button type="submit">Login</button>
+    </form>
+
+    <p style="color:red;"><?php echo $error; ?></p>
+
+    <a href="index.php">Back to homepage</a>
+</body>
+</html>
