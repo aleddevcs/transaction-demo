d9ea45de8741b55c45394fea712097bbed10b7b7
aleddevcs <aevans1@cavc.ac.uk>
2026-06-03 14:33:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<?php
+session_start();
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Transaction Demo</title>
+</head>
+<body>
+    <h1>Welcome to the Transaction Website Demo</h1>
+
+    <p>This is a simple homepage.</p>
+
+    <?php if (isset($_SESSION['username'])): ?>
+        <p>You are logged in as <?php echo $_SESSION['username']; ?>.</p>
+        <a href="dashboard.php">Go to dashboard</a>
+        <br>
+        <a href="logout.php">Logout</a>
+    <?php else: ?>
+        <a href="login.php">Login</a>
+    <?php endif; ?>
+</body>
+</html>
