e1cc6d5c3cbe2f6f19832b3c3dde129b3041e23f
aleddevcs <aevans1@cavc.ac.uk>
2026-06-03 14:34:05 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,26 @@
+<?php
+session_start();
 
+if (!isset($_SESSION["username"])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Customer Dashboard</h1>
+
+    <p>Welcome, <?php echo $_SESSION["username"]; ?>.</p>
+
+    <p>This is where orders, products or account details could appear.</p>
+
+    <a href="index.php">Home</a>
+    <br>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
